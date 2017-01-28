133cb0dfc4c260a544c269163b1204e55bc153ac
Diogo Nunes <dialex.nunes@gmail.com>
2017-01-28 15:24:41 +0000

    route middleware

--- api/server.js
+++ api/server.js
@@ -3,9 +3,9 @@
 
 // call the packages we need
 var express    = require('express');
-var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
+var app        = express();                 // define our app using express
 
 // connect to our database
 mongoose.connect('mongodb://localhost/apiDatabase');
@@ -15,24 +15,29 @@ var message = require('./models/message');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    console.log('API was called.');
+    next(); // make sure we go to the next routes and don't stop here
+});
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'Hello world! This is the API for the IoT Workshop.' });
 });
 
-// more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('/api', router);                    // all routes are prefixed with this
+
 
 // START THE SERVER
 // =============================================================================
+var port = process.env.PORT || 8080;        // set our port
 app.listen(port);
 console.log('🌈  Magic happens on port ' + port);
