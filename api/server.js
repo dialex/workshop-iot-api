84c461809882ef94f2766fadb78c897a8284cd92
Diogo Nunes <dialex.nunes@gmail.com>
2017-02-04 15:53:44 +0000

    sections

--- api/server.js
+++ api/server.js
@@ -11,18 +11,6 @@ var morgan     = require('morgan');
 var app        = express();                 // define our app using express
 let port = 8080;
 
-//db options
-let options =
-{
-    server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
-    replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } }
-};
-
-// connect to db
-mongoose.connect(config.DBHost, options);
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-
 //don't show the log when it is test
 if(config.util.getEnv('NODE_ENV') === 'dev') {
     //app.use(morgan('combined')); //uncomment for verbose console logging
@@ -35,7 +23,22 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json'}));
 
 // ============================================================================
-// ROUTES FOR OUR API
+// DATABASE SETUP
+// ============================================================================
+
+let options =
+{
+    server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } }
+};
+
+// connect
+mongoose.connect(config.DBHost, options);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// ============================================================================
+// ROUTES SETUP
 // ============================================================================
 let router = express.Router();
 
